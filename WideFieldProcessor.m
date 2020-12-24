0249e78d21be90c20d84bd8fc9516b7971e0ada0
s-acosta <acosta.santi.4@gmail.com>
2020-12-24 04:21:29 +0100

    Building of the main block
    Added the graphic methods and the computation of properties methods

--- WideFieldProcessor.m
+++ WideFieldProcessor.m
@@ -1,7 +1,7 @@
 classdef WideFieldProcessor < handle
     % WidefieldProcessor Reads fluorescence activity from multi-tif files.
     %
-    % Developed by SAM on 12/23/2020. Based on code developed by MJG and KKS. 
+    % Developed by SAM on 12/23/2020. Based on code developed by MJG and KKS.
     % Last updated 12/23/2020.
     
     properties (Access = public)
@@ -35,7 +35,7 @@ classdef WideFieldProcessor < handle
         end
         
         function getFile(obj, filename)
-           
+            
             if nargin == 2
                 obj.filename = filename;
             else
@@ -50,7 +50,7 @@ classdef WideFieldProcessor < handle
         end
         
         function setOption(obj, optionName, optionValue)
-
+            
             assert(ischar(optionName), 'First argument has to character type')
             
             error_msg = 'Invalid value for that option';
@@ -61,7 +61,7 @@ classdef WideFieldProcessor < handle
                     assert((ischar(optionValue) && contains(optionValue, 'off')) | ...
                         (isnumeric(optionValue) && ndims(optionValue) == 2), ...
                         error_msg);
-                
+                    
                 case 'Mode'
                     assert(ischar(optionValue), error_msg);
                     assert(contains(optionValue, 'auto') | ...
@@ -73,10 +73,10 @@ classdef WideFieldProcessor < handle
             end
             
             obj.Options.(optionName) = optionValue;
-                
+            
             
         end
-            
+        
     end
     
     methods (Access = private)
@@ -91,44 +91,124 @@ classdef WideFieldProcessor < handle
         
     end
     
-    % Reading methods
+    % Reading methods & pre-processing
     methods (Access = public)
         
         function readStack(obj, filename)
-                
-                disp('Reading tif stack...')
-                if nargin == 1
-                    obj.Stack = TIFFStack(obj.filename);
-                else
-                    obj.Stack = TIFFStack(filename);
-                end
-                
+            
+            disp('Reading tif stack...')
+            tic
+            if nargin == 1
+                obj.Stack = TIFFStack(obj.filename);
+            else
+                obj.Stack = TIFFStack(filename);
+            end
+            time = toc;
+            disp([' Time reading is ', time,' seconds.']);
+            
         end
         
         function computeProperties(obj)
             
             disp('Computing basic properties...')
             
+            
             switch obj.Options.Frames
                 case 'off'
-                    image_raw = single(obj.Stack(:, :, :));   
+                    image_raw = single(obj.Stack(:, :, :));
                 otherwise
                     image_raw = single(obj.Stack(:, :, obj.Options.Mode));
             end
             
             obj.avg_projection = mean(image_raw, 3);
             obj.frame_F = squeeze(mean(image_raw, [1 2]));
-            obj.f0 = median(image_raw, 3);
-         
-        end   
+            obj.f0 = obj.avg_projection;
             
+            obj.dff = 100 * (image_raw - obj.f0) ./ obj.f0 ;
+            
+        end
         
     end
     
+    % Post-Processing methods
+    methods (Access = public)
+        
+        function registerSession(obj, session_reference, mouse_reference)
+            
+            
+            
+        end
+        
+        function  maskSession(obj, mask_struct)
+            
+        end
+        
+    end
     
-        
-        
-        
+    % Graphic methods
     
+    methods
+        
+        function showPhotoBleaching(obj)
+            
+            num_frames = size(obj.Stack, 3);
+            
+            figure;
+            plot(obj.frame_F,'linewidth',2)
+            F_fit = polyfit(1:num_frames, obj.frame_F, 1);
+            PhotoBl = round((F_fit(1) * num_frames) / F_fit(2) * 100);
+            ylim([0 max(obj.frame_F) * 1.25])
+            xlabel('Frame #')
+            ylabel('Raw fluorescence')
+            set(gcf,'color',[1 1 1])
+            set(gca, 'FontSize', 14)
+            title(['Fluorescence timecourse, Photobleaching = ' ...
+                num2str(PhotoBl) '%'], 'FontSize', 18)
+            
+        end
+        
+        function showDFFMovie(obj, varargin)
+            
+            parser = inputParser;
+            addParameter(parser, 'DownSamp', 1)
+            parse(parser,varargin{:})
+            
+            down_samp = parser.Results.DownSamp;
+            num_frames = size(obj.Stack, 3);
+            max_value = 0.35 * max(obj.dff,[],'all');
+            min_value = min(obj.dff, [], 'all');
+           
+            fig = figure;
+            
+            set(fig, 'visible', 'off');
+            set(fig,'Color',[0 0 0])
+            set(fig,'Position',[100 100 525 525]);
+            subplot('Position',[0.05 0.05 0.9 0.9])
+            colormap('jet');
+            set(fig, 'visible', 'on');
+            
+            for i = 1:num_frames
+                
+                if ~ishghandle(fig)
+                    break
+                end
+                
+                if rem(i,down_samp) == 0
+                    curr_frame = obj.dff(:,:,i);
+                    imagesc(curr_frame);
+                    caxis([min_value max_value])
+                    axis square
+                    title(['Frame ' num2str(i) '/' num2str(num_frames)],...
+                        'color',[1 1 1]);
+                    
+                    drawnow()
+                end
+
+            end
+            
+        end
+        
+        
+    end
     
 end
